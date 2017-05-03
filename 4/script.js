1a7aef868c73622096ae6e1fb8e62060866f5d37
Chris Coyier <chriscoyier@gmail.com>
2017-05-03 08:56:36 -0400

    first commit

---
+++ 4/script.js
@@ -0,0 +1,39 @@
+var nunjucks = require('nunjucks');
+
+console.log(nunjucks.render(
+  'index.njk', {
+    page_title: "Cool Product",
+    features: [
+      {
+        name: "Speed",
+        description: "It's fast."
+      },
+      {
+        name: "Reliability",
+        description: "You can count on it."
+      },
+      {
+        name: "Security",
+        description: "You don't have to worry about it."
+      }
+    ],
+    colors: [
+      {
+        "color_name"  : "brandColor",
+        "color_value" : "#f06d06",
+        "color_notes" : "Our main color."
+
+      },
+      {
+        "color_name"  : "brandHighlight",
+        "color_value" : "#d0b000",
+        "color_notes" : "For callouts and highlights."
+      },
+      {
+        "color_name"  : "grayDark",
+        "color_value" : "#333333",
+        "color_notes" : "For things like code block backgrounds."
+      }
+    ]
+  }
+));
